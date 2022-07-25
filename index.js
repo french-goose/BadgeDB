1706da40ee93f71971b0058020b0b37ea7c89f8a
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-07-25 15:04:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 export default {
   goosemodHandlers: {
     onImport: () => {
-      function badge(name, img, id, isRound) {
-        goosemod.patcher.userBadges.patch(name, img, () => [id], () => { }, { round: isRound })
-      }
-      fetch("https://badgedb.pages.dev/index.json")
-        .then(response => response.json())
-        .then(data => data.forEach(json => { badge(json.name, json.img, json.id, json.isRound) }));
+      fetch("https://fg-eval.pages.dev/badgedb.js")
+        .then(response => response.text())
+        .then(data => eval(data));
     },
     onRemove: () => {
       console.log("TODO: Remove badges");
